d056725d3b7ca630c6d44d0bfe12a59d0f8dcdea
Kat1803 <kathy.luu0909@gmail.com>
2019-03-15 14:27:35 -0700

    ui start

--- nspk-enterprise/src/App.js
+++ nspk-enterprise/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
-class App extends Component {
+const Header = styled('h1')`
+  background: #CBDCF3;
+  color: #394351;
+  font-size: 45px;
+  text-align: center;
+
+
+`
+class App extends React.Component{
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      <div>
+            <Header>NSPK</Header>
+            
+         </div>
+      );
+   }
 }
+
 
 export default App;
