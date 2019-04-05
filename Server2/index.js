f55658d5220316db5179e8e13d69268912526ad5
steve <steven9ngo@gmail.com>
2019-04-05 15:23:21 -0700

    2

--- Server2/index.js
+++ Server2/index.js
@@ -4,14 +4,15 @@ var http = require('http');
 var fs = require('fs');
 // Create a service (the app object is just a callback).
 var app = express();
-const mysql = require('mysql');
+var employee = require ('./routes/employee')
+var admin = require("./routes/admin");
 
 // Auth0 API protect
 // https://auth0.com/blog/react-tutorial-building-and-securing-your-first-app/
 // Auth0 bobgel12@gmail.com
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
-const checkJwt = jwt({
+export const checkJwt = jwt({
 	secret: jwksRsa.expressJwtSecret({
 	  cache: true,
 	  rateLimit: true,
@@ -27,33 +28,30 @@ const checkJwt = jwt({
 
 // Use checkJwt as a middleware 
 
-// Dung Code
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Steven Code
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 
-// Might remove later since we use react for the Client
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Might remove later since we use react for the Client
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'jsx');
+// app.engine('jsx', require('express-react-views').createEngine());
+
 
 // Routes
+app.use("/employees", employee);
+app.use("/admin", admin);
 
-app.get("/employees/:employeeid", checkJwt, usersRouter);
-app.get("/admin/getallemployees/:from", checkJwt, indexRouter);
-app.get("/admin/get1employees/:employeeid", checkJwt, indexRouter);
-app.post("/admin/updateinfo/", checkJwt, indexRouter);
+// End Steven Code
 
-
-// End Dung Code
 
 // This line is from the Node.js HTTPS documentation.
 var options = {
---
+++ Server2/routes/admin.js
@@ -0,0 +1,126 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require("../index.js");
+const database = require("../database.js");
+
+
+router.get("/getallemployees/:from", checkJwt, function(req, res) {
+  database.connection.connect();
+  let query = `SELECT * FROM employees Limit ${req.params.employeeid}, 100; `;
+  database.connection.query(query, function(error, results, fields) {
+    //if error, print blank results
+    if (error) {
+      // console.log(error);
+      var apiResult = {};
+
+      apiResult.meta = {
+        table: section,
+        type: "collection",
+        total: 0
+      };
+      //create an empty data table
+      apiResult.data = [];
+
+      //send the results (apiResult) as JSON to Express (res)
+      //Express uses res.json() to send JSON to client
+      //you will see res.send() used for HTML
+      res.json(apiResult);
+    }
+
+    //make results
+    var resultJson = JSON.stringify(results);
+    resultJson = JSON.parse(resultJson);
+    var apiResult = {};
+
+    // create a meta table to help apps
+    //do we have results? what section? etc
+    apiResult.meta = {
+      table: section,
+      type: "collection",
+      total: 1,
+      total_entries: 0
+    };
+
+    //add our JSON results to the data table
+    apiResult.data = resultJson;
+
+    //send JSON to Express
+    res.json(apiResult);
+  });
+
+   database.connection.end();
+});
+
+
+router.get("/get1employees/:employeeid", checkJwt, function(req, res) {
+  database.connection.connect();
+  let query = `SELECT employees.emp_no, employees.birth_date , employees.first_name,
+                employees.last_name, employees.gender, employees.hire_date, title.title, salary.salary 
+                FROM ((employees
+                INNER JOIN (
+                  SELECT emp_no, title
+                  FROM titles
+                  WHERE emp_no = ${req.params.employeeid}
+                  ORDER BY from_date DESC
+                  Limit 1
+                )title ON employees.emp_no = title.emp_no)
+                INNER JOIN (
+                  SELECT emp_no, salary
+                  FROM salaries
+                  WHERE emp_no = ${req.params.employeeid}
+                  ORDER BY from_date DESC
+                  Limit 1
+                )salary ON employees.emp_no = salary.emp_no);`;
+
+  database.connection.query(query, function(error, results, fields) {
+    //if error, print blank results
+    if (error) {
+      // console.log(error);
+      var apiResult = {};
+
+      apiResult.meta = {
+        table: section,
+        type: "collection",
+        total: 0
+      };
+      //create an empty data table
+      apiResult.data = [];
+
+      //send the results (apiResult) as JSON to Express (res)
+      //Express uses res.json() to send JSON to client
+      //you will see res.send() used for HTML
+      res.json(apiResult);
+    }
+
+    //make results
+    var resultJson = JSON.stringify(results);
+    resultJson = JSON.parse(resultJson);
+    var apiResult = {};
+
+    // create a meta table to help apps
+    //do we have results? what section? etc
+    apiResult.meta = {
+      table: section,
+      type: "collection",
+      total: 1,
+      total_entries: 0
+    };
+
+    //add our JSON results to the data table
+    apiResult.data = resultJson;
+
+    //send JSON to Express
+    res.json(apiResult);
+  });
+
+  database.connection.end();
+});
+
+
+router.post("/updateinfo", checkJwt, function(req, res) {
+  res.send("GET request to the homepage");
+});
+
+module.exports = router;
+
+
