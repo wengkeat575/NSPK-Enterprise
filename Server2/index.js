4b92139b4db82f4b9c61329bf4c38afc9c8c07aa
Phuc Le <Bobgel12@gmail.com>
2019-04-09 11:19:41 -0700

    Protect APi

--- Server2/index.js
+++ Server2/index.js
@@ -1,30 +1,71 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-// Create a service (the app object is just a callback).
-var app = express();
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
-// Auth0 API protect
-// https://auth0.com/blog/react-tutorial-building-and-securing-your-first-app/
-// Auth0 bobgel12@gmail.com
+// Start Auth0 API protect
+const express = require('express');
 const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
-const checkJwt = jwt({
-	secret: jwksRsa.expressJwtSecret({
-	  cache: true,
-	  rateLimit: true,
-	  jwksRequestsPerMinute: 5,
-	  jwksUri: `https://<YOUR_AUTH0_DOMAIN>/.well-known/jwks.json`
-	}),
-  
-	// Validate the audience and the issuer.
-	audience: '<YOUR_AUTH0_CLIENT_ID>',
-	issuer: `https://<YOUR_AUTH0_DOMAIN>/`,
-	algorithms: ['RS256']
-  });
+const cors = require('cors');
 
-// Use checkJwt as a middleware 
+// Create a service (the app object is just a callback).
+const app = express();
+require('dotenv').config();
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
+}
+
+const corsOptions =  {
+  origin: 'http://localhost:3000'
+};
+
+app.use(cors(corsOptions));
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+const checkScopes = jwtAuthz(['read:messages']);
+
+// Sample public 
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
+app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  return res.status(err.status).json({ message: err.message });
+});
+
+
+// End Auth0 Use checkJwt as a middleware 
 
 // Dung Code
 var indexRouter = require('./routes/index');
