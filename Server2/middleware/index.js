fc7f65ccea5e955ec4f26e7a5a159ac85453c2b9
Phuc Le <Bobgel12@gmail.com>
2019-05-01 15:25:27 -0700

    create modal

--- Server2/middleware/index.js
+++ Server2/middleware/index.js
@@ -6,7 +6,7 @@ middleware.isThisYourAccount = function (req, res, next){
 	// if (req.isAuthenticated()) {
 	// 	Post.findById(req.params.id, function(err, foundPost){
 	// 		if (foundPost.author.id.equals(req.user._id)) {
-				// next();
+				next();
 	// 		} else{
 	// 			req.flash("error","You don't have permission to do that");
 	// 			res.redirect("back");
@@ -21,7 +21,7 @@ middleware.isAdmin = function(req, res, next){
 	// if (req.isAuthenticated()) {
 	// 	Comment.findById(req.params.commentId, function(err, foundComment){
 	// 		if (foundComment.author.id.equals(req.user._id)) {
-				// next();
+				next();
 	// 		} else{
 	// 			req.flash("error", "You don't have permission to do that!");
 	// 			res.redirect("back");
--- nspk-enterprise/src/Profile.js
+++ nspk-enterprise/src/Profile.js
@@ -7,9 +7,69 @@ import TopBar from "./TopBar";
 import ConnectForm from "./ConnectForm";
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+
 const style = {
   marginLeft: 20
 };
+const styles = theme => ({
+	paper: {
+	  position: 'absolute',
+	  width: theme.spacing.unit * 50,
+	//   backgroundColor: theme.palette.background.paper,
+	//   boxShadow: theme.shadows[5],
+	  padding: theme.spacing.unit * 4,
+	  outline: 'none',
+	},
+  });
+  
+class SimpleModal extends React.Component {
+	state = {
+	  open: false,
+	};
+  
+	handleOpen = () => {
+	  this.setState({ open: true });
+	};
+  
+	handleClose = () => {
+	  this.setState({ open: false });
+	};
+  
+	render() {
+	  const { classes } = this.props;
+  
+	  return (
+		<div>
+		  <Button variant="contained" size="large" color="secondary" onClick={this.handleOpen}>
+              Connect with Database
+          </Button>
+		  <Modal
+			aria-labelledby="simple-modal-title"
+			aria-describedby="simple-modal-description"
+			open={this.state.open}
+			onClose={this.handleClose}
+		  >
+			<div className={classes.paper} style={{top: `${20}%`,left: `${50}%`,transform: `translate(-${50}%, -${50}%)`}}>
+			<ConnectForm/>
+			</div>
+		  </Modal>
+		</div>
+	  );
+	}
+  }
+  
+  SimpleModal.propTypes = {
+	classes: PropTypes.object.isRequired,
+  };
+  
+  // We need an intermediary variable for handling the recursive nesting.
+  const SimpleModalWrapped = withStyles(styles)(SimpleModal);
+  
+
 
 class Profile extends React.Component {
   constructor(props) {
@@ -44,26 +104,27 @@ class Profile extends React.Component {
     console.log(this.state.editMode);
     return (
       <div>
-		<Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" {...other}>
+		{/* <Dialog onClose={()=> alert("Closed")} aria-labelledby="simple-dialog-title">
 	        <DialogTitle id="simple-dialog-title">Connect to your account</DialogTitle>
 	        <div>
 				<ConnectForm/>
 	        </div>
-	      </Dialog>
+		  </Dialog> */}
+		  
       <Paper
         style={{
-          backgroundImage: "url(" + require("./technology.jpg") + ")",
-          height: "100vh"
+			backgroundImage: "url(" + require("./technology.jpg") + ")",
+			height: "100vh"
         }}
-      >
+		>
         <Paper
           style={{
-            margin: "48px",
-            padding: "32px",
-            margin: "auto",
-            width: "480px"
-          }}
-        >
+			  margin: "48px",
+			  padding: "32px",
+			  margin: "auto",
+			  width: "480px"
+			}}
+			>
 
           <React.Fragment>
             <Grid
@@ -72,7 +133,7 @@ class Profile extends React.Component {
               justify="center"
               alignItems="center"
               spacing={24}
-            >
+			  >
             <h2>Employee Profile</h2>
               <Grid item xs={8} md={6}>
                 <TextField
@@ -82,7 +143,7 @@ class Profile extends React.Component {
                   id="Full Name"
                   label="Employee Full Name"
                   fullWidth
-                />
+				  />
               </Grid>
               <Grid item xs={8} md={6}>
                 <TextField
@@ -141,16 +202,18 @@ class Profile extends React.Component {
             marginLeft: "50"
           }}
         >
+		  <SimpleModalWrapped/>
           {this.state.editMode ? (
-            <Button variant="contained" size="large" color="primary">
+            <Button variant="contained" size="large" color="secondary">
               Save
             </Button>
           ) : (
             <Button
-              variant="contained"
-              size="large"
-              justifyContent= "center"
-              alignItems= "center"
+				variant="contained" size="large" color="secondary"
+            //   variant="contained"
+            //   size="large"
+            //   justifyContent= "center"
+            //   alignItems= "center"
               onClick={this.handleEdit.bind(this)}
             >
               Edit
