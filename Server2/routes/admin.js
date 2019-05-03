a948dbe5d8b744a29b6b395f93a9c9b40ab28767
Kat1803 <kathy.luu0909@gmail.com>
2019-05-03 01:58:52 -0700

    test

--- Server2/routes/admin.js
+++ Server2/routes/admin.js
@@ -49,8 +49,13 @@ router.get("/getallemployees/:from", middleware.isAdmin, function(req, res) {
   });
 });
 
+<<<<<<< HEAD
 //checkJwt, middleware.isAdmin,
 router.get("/get1employees/:employeeid",  function(req, res) {
+=======
+
+router.get("/get1employees/:employeeid", checkJwt, middleware.isAdmin, function(req, res) {
+>>>>>>> parent of 3860ca7... Add salary function
 // router.get("/get1employees/:employeeid", function(req, res) {
 
   const query = `SELECT employees.emp_no, employees.birth_date , employees.first_name,
